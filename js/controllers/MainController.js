9c1d0818d04117b5766f5832061c4517dc6548a2
Daði Sigursveinn Harðarson <dadi.sigursveinn@gmail.com>
2015-05-06 14:11:58 +0000

    Final_v1
    v1 ready

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,3 +1,22 @@
+//http://stackoverflow.com/questions/1531093/how-to-get-current-date-in-javascript
+var today = new Date();
+var dd = today.getDate()-1;
+var mm = today.getMonth()+1; //January is 0!
+var yyyy = today.getFullYear();
+
+if(dd<10) {
+    dd='0'+dd
+} 
+
+if(mm<10) {
+    mm='0'+mm
+} 
+
+today = yyyy+'/'+mm+'/'+dd;
+document.write(today);
+
+
+
 app.controller('MainController', ['$scope', 'gasPrice', function($scope, gasPrice) { //MainController-inn sem notar $scope og gasPrice (sem sækir gögn í API). Posts injectað í main controller til þess að senda gögn í database
 	gasPrice.success(function(data) { 
 	    $scope.stations = data; //$scope.stations fær data-ð úr api-inu
@@ -31,12 +50,14 @@ app.controller('MainController', ['$scope', 'gasPrice', function($scope, gasPric
 	[
 	];
 	localStorage.setItem('posts', JSON.stringify($scope.posts));*/
+	
 
+	
 
 	$scope.addPost = function(){//addPost er function til þess að skrá nýjar dælingar
 		if(!$scope.stationInput || $scope.stationInput === '') { return; }
 		if(!$scope.litersInput || $scope.litersInput === '') { return; }
-		if(!$scope.dateInput || $scope.dateInput === '') { return; }//kemur í veg fyrir að hægt sé að posta með engu info
+		//if(!$scope.noteInput || $scope.noteInput === '') { return; }//kemur í veg fyrir að hægt sé að posta með engu info
 	    /*$scope.posts.push({
 	    	id: localStorage.length,
 	    	station: $scope.stationInput, 
@@ -47,11 +68,20 @@ app.controller('MainController', ['$scope', 'gasPrice', function($scope, gasPric
 	    $scope.dateInput = '';
 	    //localStorage.setItem('posts', JSON.stringify($scope.posts));*/
 
+	    var priceAtStation = 0;
+		$scope.stations.results.forEach(function(station){
+			if(station.flag_value === $scope.stationInput){
+				priceAtStation = station.ver_number;
+			}
+		})
+
 		var newPost = {
 				id: localStorage.length,
 		    	station: $scope.stationInput, 
 		    	liters: $scope.litersInput,
-		    	date: $scope.dateInput 
+		    	note: $scope.noteInput, 
+		    	date: today,
+		    	price: (priceAtStation * $scope.litersInput)
 			}
 
 		// Add contact object to localStorage as the value to a new property
@@ -61,11 +91,11 @@ app.controller('MainController', ['$scope', 'gasPrice', function($scope, gasPric
 		$scope.posts.push( newPost );
 
 		$scope.litersInput = '';//Cleara textboxin
-		$scope.dateInput = '';
+		$scope.noteInput = '';
 
 	};
 
-	$scope.deletePost = function(index, item) {
+	/*$scope.deletePost = function(index, item) { // Náði ekki að láta þetta virka
 
         // index param is an ngRepeat variable
         // docs.angularjs.org/api/ng/directive/ngRepeat
@@ -74,9 +104,11 @@ app.controller('MainController', ['$scope', 'gasPrice', function($scope, gasPric
         localStorage.removeItem( 'item' + item.id );
 
         // Remove item from the contacts array
-        $scope.posts.splice( index, 1 );
+        console.log(index);
+        console.log(item.id);
+        $scope.posts.splice( item.id-1, 1 );
 
-    }
+    }*/
 
 
 	$scope.save = function(){
